893e92ade638e4a95dd2134cad7bc72e3f48be87
KARBOU Aristide <aristidetiger12@gmail.com>
2024-01-06 15:54:22 +0000

    Adding meta data to pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,8 +15,29 @@ export async function generateMetadata(): Promise<Metadata> {
   const client = createClient();
   const page = await client.getSingle("homepage");
 
+  console.log(page.data.meta_image.url);
+  const url = page.data.meta_image.url;
+
   return {
     title: page.data.meta_title,
     description: page.data.meta_description,
+    metadataBase: new URL("https://aristide-karbou.vercel.app"),
+    openGraph: {
+      type: "website",
+      locale: "en_US",
+      url: "https://aristide-karbou.vercel.app/",
+      siteName: "Aristide Karbou",
+      images: [
+        {
+          url: page.data.meta_image.url ?? "",
+          width: page.data.meta_image.dimensions?.width,
+          height: page.data.meta_image.dimensions?.height,
+          alt: page.data.meta_image.alt?? "",
+        },
+      ],
+      
+    },
+    authors: [{ name: "Aristide KARBOU", url: "https://aristide-karbou.vercel.app/" }],
+    keywords: "developer, aristide , karbou , react, nextjs, typescript, javascript",
   };
 }
--- src/app/project/[uid]/page.tsx
+++ src/app/project/[uid]/page.tsx
@@ -24,10 +24,28 @@ export async function generateMetadata({
     .getByUID("project", params.uid)
     .catch(() => notFound());
 
-  return {
-    title: page.data.meta_title,
-    description: page.data.meta_description,
-  };
+    return {
+      title: page.data.meta_title,
+      description: page.data.meta_description,
+      metadataBase: new URL("https://aristide-karbou.vercel.app"),
+      openGraph: {
+        type: "website",
+        locale: "en_US",
+        url: "https://aristide-karbou.vercel.app/",
+        siteName: "Aristide Karbou",
+        images: [
+          {
+            url: page.data.meta_image.url ?? "",
+            width: page.data.meta_image.dimensions?.width,
+            height: page.data.meta_image.dimensions?.height,
+            alt: page.data.meta_image.alt?? "",
+          },
+        ],
+        
+      },
+      authors: [{ name: "Aristide KARBOU", url: "https://aristide-karbou.vercel.app/" }],
+      keywords: "developer, aristide , karbou , react, nextjs, typescript, javascript",
+    };
 }
 
 export async function generateStaticParams() {
