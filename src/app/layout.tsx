62b23caee42a35dcc1a374108997a37be34469c0
KARBOU Aristide <aristidetiger12@gmail.com>
2024-01-23 22:17:16 +0000

    Google analitics config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,14 @@
+/* eslint-disable @next/next/inline-script-id */
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import { createClient, repositoryName } from "@/prismicio";
+import { ThemeProvider } from "@/theme/ThemeProvider";
+import { PrismicPreview } from "@prismicio/next";
+import clsx from "clsx";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
-import clsx from "clsx";
-import { ThemeProvider } from "@/theme/ThemeProvider";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
-import { PrismicPreview } from "@prismicio/next";
-import { createClient, repositoryName } from "@/prismicio";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -30,6 +32,21 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+      </Script>
       <head />
       <body
         className={clsx(
@@ -38,7 +55,7 @@ export default function RootLayout({
         )}
       >
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
-          <div className="background-gradient absolute  inset-0 -z-50 w-full h-full">
+          <div className="background-gradient absolute inset-0 -z-50 h-full w-full">
             <div className=" m-96"></div>
           </div>
           <div className="pointer-events-none absolute inset-0 -z-40 h-full bg-[url('/bg.png')] opacity-90 mix-blend-soft-light"></div>
